8c3f661337f08a284e79138bee63fb52eecf460f
sikjjang <sooso1648@hotmail.com>
2015-11-29 20:26:26 +0900

    somethings

--- blog/views.py
+++ blog/views.py
@@ -62,7 +62,7 @@ def add_comment_to_post(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
         form = CommentForm(request.POST)
-        if form.is_vaild():
+        if form.is_valid():
             comment = form.save(commit=False)
             comment.post = post
             comment.save()
@@ -70,3 +70,16 @@ def add_comment_to_post(request, pk):
     else:
         form = CommentForm()
     return render(request, 'blog/add_comment_to_post.html', {'form': form})
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('blog.views.post_detail', pk=comment.post.pk)
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('blog.views.post_detail', pk=post_pk)
